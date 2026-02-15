790e9a74540cfc63ce564d3cadf3a6cedd0bb386
Soraya Schwarzenecker <sorayaschwarzenecker@Sorayas-MacBook-Air.local>
2026-02-15 13:11:33 -0500

    vercel analytics

--- App.tsx
+++ App.tsx
@@ -6,11 +6,13 @@ import Portfolio from './components/Portfolio';
 import Acting from './components/Acting';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className="font-sans text-slate-900 bg-white selection:bg-pastel-200 selection:text-pastel-900">
       <Header />
+      <Analytics/>
       <main>
         <Hero />
         <About />
