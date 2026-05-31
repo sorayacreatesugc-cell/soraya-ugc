f976fe488d12a1a9894267219aa327c3393f1121
Jon Sundin <jon@Jons-M4-MacBook-Air.local>
2026-05-31 13:19:36 -0400

    video label names changed

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -17,42 +17,42 @@ const videoItems = [
     src: 'https://img.youtube.com/vi/_P9ekwqLHzI/maxresdefault.jpg', 
     link: 'https://youtube.com/shorts/_P9ekwqLHzI?feature=share',
     caption: 'UGC Short Example', 
-    label: 'Travel - Voiceover / B-Roll'
+    label: 'Travel Vlog Style'
   },
     {
     id: 3,
     src: 'https://img.youtube.com/vi/XkLyonNBTJ8/maxresdefault.jpg',
     link: 'https://youtube.com/shorts/XkLyonNBTJ8?feature=share',
     caption: 'Nerd Wallet UGC',
-    label: 'Finances'
+    label: 'Conversational Testimonial'
   },
   { 
     id: 4, 
     src: 'https://img.youtube.com/vi/_x627w4h9l8/maxresdefault.jpg', 
     link: 'https://youtube.com/shorts/_x627w4h9l8?feature=share',
     caption: 'App Showcase', 
-    label: 'Skits'
+    label: 'Skit'
   },
   { 
     id: 5, 
     src: 'https://img.youtube.com/vi/eNpEmXCd18k/maxresdefault.jpg', 
     link: 'https://youtube.com/shorts/eNpEmXCd18k?feature=share',
     caption: 'Fashion Haul', 
-    label: 'Sport & Fashion'
+    label: 'Get Ready With Me - Dance'
   },
   { 
     id: 6, 
     src: 'https://img.youtube.com/vi/27aA1428Gkc/maxresdefault.jpg', 
     link: 'https://youtube.com/shorts/27aA1428Gkc?feature=share',
     caption: 'Product Demo', 
-    label: 'Home'
+    label: 'Product Review'
   },
   { 
     id: 7, 
     src: 'https://img.youtube.com/vi/nuz3XjctcNU/maxresdefault.jpg', 
     link: 'https://youtube.com/shorts/nuz3XjctcNU?feature=share',
     caption: 'Aesthetic Vlog', 
-    label: 'Lifestyle'
+    label: 'Night Routine'
   },
   // {
   //   id: 6,
